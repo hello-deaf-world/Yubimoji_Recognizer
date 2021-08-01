5eeb85d75b3902ebed590306eb1d002ef6bb1bb7
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2021-08-01 15:49:44 +0900

    特定のディレクトリから画像ファイルを読み込む関数を導入．あとpyファイルのカレントディレクトリの取得コードも書いたので我流だけど参考にしてください．

--- yubimoji_recognizer/src/yubimoji_recognize.py
+++ yubimoji_recognizer/src/yubimoji_recognize.py
@@ -1,6 +1,15 @@
+#-----------------------------------------------------
+# ここで杉村流の，このPythonファイルのカレントディレクトリの取得方法
+import inspect
+import os
+import sys
+PYPATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) + "/"
+#-----------------------------------------------------
+import time
 import cv2
 import mediapipe as mp
-from image import IMAGE_FILES
+# from image import IMAGE_FILES
+from image import get_images
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
@@ -14,6 +23,7 @@ with mp_hands.Hands(
   # for i in range(len(files_name)):
   #   print("files name:", files_name[i])  
   
+  IMAGE_FILES = get_images(PYPATH)
   for idx, file in enumerate(IMAGE_FILES):
     
     # Read an image, flip it around y-axis for correct handedness output (see
@@ -36,26 +46,38 @@ with mp_hands.Hands(
           f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
           f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})'
       )
+      # ここでランドマーク(骨格ぽいんた？)の，画像への描写してる
       mp_drawing.draw_landmarks(
           annotated_image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-    cv2.imwrite(
-        '/tmp/annotated_image' + str(idx) + '.png', cv2.flip(annotated_image, 1))
+    # ここで画像の保存してる？
+    # /tmpってWindowsでどこやねんｗ
+    # cv2.imwrite(
+    #     '/tmp/annotated_image' + str(idx) + '.png', cv2.flip(annotated_image, 1))
+
+    # 画像の保存ではなく表示の方でためそう
+    ## => OKそう
+    while True:
+      cv2.imshow('MediaPipe Hands', annotated_image)
+      if cv2.waitKey(5) & 0xFF == 27:
+        # ここをやると一枚ずつ切り替わる
+        break
+      time.sleep(1)
 
 # # For webcam input:
-cap = cv2.VideoCapture(1)   #REN...camera number 1 Yuri...0
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FOURCC, 0x32595559)
-cap.set(cv2.CAP_PROP_FPS, 60)
-with mp_hands.Hands(
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
-  while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-      print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
-      continue
+# cap = cv2.VideoCapture(1)   #REN...camera number 1 Yuri...0
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+# cap.set(cv2.CAP_PROP_FOURCC, 0x32595559)
+# cap.set(cv2.CAP_PROP_FPS, 60)
+# with mp_hands.Hands(
+#     min_detection_confidence=0.5,
+#     min_tracking_confidence=0.5) as hands:
+#   while cap.isOpened():
+#     success, image = cap.read()
+#     if not success:
+#       print("Ignoring empty camera frame.")
+#       # If loading a video, use 'break' instead of 'continue'.
+#       continue
 
 #     # Flip the image horizontally for a later selfie-view display, and convert
 #     # the BGR image to RGB.
