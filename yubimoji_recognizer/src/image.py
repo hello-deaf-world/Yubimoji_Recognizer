255eee4b8d10d818a8e1bc57609d890a9da94072
kappakun17 <ren.programing@gmail.com>
2021-08-01 23:20:17 +0900

    Japanese_list.pyにあるja_listから、リストを追加した分、ディレクトリを作成するシステムを実装しました

--- yubimoji_recognizer/src/image.py
+++ yubimoji_recognizer/src/image.py
@@ -7,6 +7,12 @@ PYPATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())
 #-----------------------------------------------------
 import glob
 
+# 50音それぞれのフォルダを作成する
+#---------------------------------------------
+from Japanese_list import ja_list
+#---------------------------------------------
+
+
 #読み込む写真のリスト
 #読み込んだらコメントアウトすること
 
@@ -27,3 +33,21 @@ def get_images(ROOT_PATH):
 
 # 動作確認用
 # get_images(PYPATH)
+
+# ディレクトリを作成
+# ja_listから名前を取得
+# ja_listに追加するとその分追加ディレクトリを作成できる
+def make_dir():
+    for i in range(len(ja_list)):
+        path = 'images\\{path}'.format(path= str(i) + "." + ja_list[i])
+        
+        if check_dir(path) == False: os.mkdir(path)
+        continue
+
+# ファイル名がかぶっていたらFalse かぶってなかったらTrueを返す
+def check_dir(path):
+    return os.path.isdir(path)
+
+#ディレクトリ作成実行
+make_dir()
+
