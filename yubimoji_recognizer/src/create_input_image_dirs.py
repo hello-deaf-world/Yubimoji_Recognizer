294d0e9eceb4bb5e6b088f98c12687b45546fb16
kappakun17 <ren.programing@gmail.com>
2021-08-05 20:34:26 +0900

    ディレクトリ作成システムをここのファイルに移動しました

---
+++ yubimoji_recognizer/src/create_input_image_dirs.py
@@ -0,0 +1,15 @@
+from Japanese_list import ja_list
+from image import make_dir
+import os
+
+
+# ディレクトリを作成
+# ja_listから名前を取得
+# ja_listに追加するとその分追加ディレクトリを作成できる
+def make_dir():
+    for i in range(len(ja_list)):
+        path = 'images\\{path}'.format(path= str(i) + "." + ja_list[i])
+        os.makedirs(path)
+
+
+make_dir()
