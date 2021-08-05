09ef2e058e5a8c7cd46140103c96988678eb4796
kappakun17 <ren.programing@gmail.com>
2021-08-05 20:35:44 +0900

    ファイルの読み込みシステムを修正しました（.png拡張子指定付き）

--- yubimoji_recognizer/src/image.py
+++ yubimoji_recognizer/src/image.py
@@ -25,50 +25,18 @@ IMAGE_FILES = [
 ]
 
 def get_images(ROOT_PATH):
-
-    pathList = []
-
+    
     DIR_NAME = "{root}".format(
-        root = ROOT_PATH, )
+        root = ROOT_PATH)
 
-    #上記ディレクトリにある画像ファイル群のファイル名リストを取得
-    for i in range(len(ja_list)):
-        files = glob.glob(ROOT_PATH + "images/" + str(i)+"."+ja_list[i]+"/*")
-        #ディレクトリをpathListに追加
-        for file in files:
-            pathList.append(file)
-
-    return pathList
+    #上記ディレクトリにある画像ファイル群のファイル名リストを取得(拡張子.png)
+    
+    images = glob.glob(ROOT_PATH + "images//*//*.png")
+    return images
             
-            
-
-            # test print -> succsess
-            # print(file)
-
-    # DIR_NAME = "{root}/images//0.a".format(
-    #     root = ROOT_PATH, )
-        
-    #     #上記ディレクトリにある画像ファイル群のファイル名リストを取得
-    # return glob.glob("{path}/*".format(path = DIR_NAME))
 
 # 動作確認用
-# print(get_images(PYPATH))
+print(get_images(PYPATH))
 
-# ディレクトリを作成
-# ja_listから名前を取得
-# ja_listに追加するとその分追加ディレクトリを作成できる
-def make_dir():
-    for i in range(len(ja_list)):
-        path = 'images\\{path}'.format(path= str(i) + "." + ja_list[i])
-        
-        if check_dir(path) == False: os.mkdir(path)
-        continue
 
-# ファイル名がかぶっていたらFalse かぶってなかったらTrueを返す
-def check_dir(path):
-    return os.path.isdir(path)
-
-#ディレクトリ作成実行
-#↓↓↓実行するときはコメント外す↓↓
-# make_dir()
 
