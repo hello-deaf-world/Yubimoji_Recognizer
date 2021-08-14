45fa0ec0c0b6113f5d1dc6cddeee3f7df1f85f28
kappakun17 <ren.programing@gmail.com>
2021-08-14 23:01:32 +0900

    csvファイル初期作成

---
+++ yubimoji_recognizer/src/create_csv_file.py
@@ -0,0 +1,28 @@
+import csv
+import glob
+import inspect
+import os
+import sys
+PYPATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) + "/"
+
+
+ROOT_PATH = PYPATH
+
+#csvファイル指定
+# filename = "D:\Hello_Deaf_World\pro-yubimoji-_recognizer\Github\Yubimoji_Recgnizer\yubimoji_recognizer\src\yubimoji_data.csv"
+
+
+#CSV作成
+def makeCSV(filename):
+    hedder = ["Hiragana", "Romaji" , "hand_labels" , "hand_landmarks" ]
+    
+
+    with open(filename, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(hedder)
+
+makeCSV('yubimoji_data.csv')
+
+
+                
+
