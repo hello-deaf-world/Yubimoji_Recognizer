d51a9cd97741af6911a7111108a3fdd68b048c4b
kappakun17 <ren.programing@gmail.com>
2021-09-04 01:32:55 +0900

    renamed_imagesとnew_imagesフォルダそれぞれの中でフォルダを作成することができます。ja_dictへ新しく登録したキーがあれば、追加でフォルダを作成することもできます。

--- yubimoji_recognizer/src/create_input_image_dirs.py
+++ yubimoji_recognizer/src/create_input_image_dirs.py
@@ -6,13 +6,33 @@ import os
 # ディレクトリを作成
 # ja_dictから名前を取得
 # ja_dictに追加するとその分追加ディレクトリを作成できる
-def make_dir():
+def make_dir_renamed_images():
+    
     count = 0
     for name in ja_dict.values():
-        path = 'images\\{path}'.format(path= str(count) + "_" + name)
-        
+        renamed_images_path = 'renamed_images\\{path}'.format(path= str(count) + "_" + name)
            
-        os.makedirs(path,exist_ok=True)
+        os.makedirs(renamed_images_path,exist_ok=True)
         count += 1
 
-make_dir()
+        path = renamed_images_path + '\\' + name + '_memo.txt'
+        with open(path,'w', encoding = 'utf-8') as f:
+            f.write('変更した'+ name + 'ラベルの画像データを格納しています。')
+            
+
+def make_dir_new_images():
+
+    count = 0
+    for name in ja_dict.values():
+        new_images_path = 'new_images\\{path}'.format(path= str(count) + "_" + name)
+
+        os.makedirs(new_images_path, exist_ok=True)
+        count += 1
+
+        path = new_images_path + '\\' + name + '_memo.txt'
+        with open(path, 'w', encoding = 'utf-8') as f:
+            f.write('変更する前の'+ name + 'ラベルの画像データを格納しています。')
+
+
+make_dir_renamed_images()
+make_dir_new_images()
