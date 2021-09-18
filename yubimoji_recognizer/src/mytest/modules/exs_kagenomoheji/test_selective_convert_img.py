c1880e0424f0102ebe776ae665aff9d13af0143f
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2021-09-19 02:24:31 +0900

    画像の類似度を算出するコードのテストのための画像作りのために色々いじった

--- yubimoji_recognizer/src/mytest/modules/exs_kagenomoheji/test_selective_convert_img.py
+++ yubimoji_recognizer/src/mytest/modules/exs_kagenomoheji/test_selective_convert_img.py
@@ -16,38 +16,21 @@ from modules.exs_kagenomoheji.img_converter import (
 selective_convert_img(
     [
         {
-            # cv2.imreadでBGRまたはBGRAと見なされてしまうので，まず戻す必要あり
+            # cv2.imreadでBGRまたはBGRAと見なされてしまうので，まず戻す必要あり(不要かも)
             "method": ImgConvertMethod.CVTCOLOR,
             "params": {
                 "mode": CV2CvtColorMode.BGR2RGB
             }
         },
-        # {
-        #     "method": ImgConvertMethod.CVTCOLOR,
-        #     "params": {
-        #         "mode": CV2CvtColorMode.RGB2GRAY
-        #     }
-        # },
-        {
-            "method": ImgConvertMethod.BGR_GAMMA_GRAY,
-            "params": {
-                "ret_uint8": True
-            }
-        },
-        # {
-        #     "method": ImgConvertMethod.DECOLOR,
-        #     "params": {}
-        # },
         # OK: SCALE/RESIZE/MOSAIC/CVTCOLOR/BLUR_AVG/BLUR_MED/BLUR_GAUSSIAN/BLUR_BILATERAL(激重)/INVERT/THRESHOLD/BGR_GAMMA_GRAY/BGR_SAT_BRT/DECOLOR/CVTGAMMA
         {
-            "method": ImgConvertMethod.CVTGAMMA,
+            "method": ImgConvertMethod.CVTCOLOR,
             "params": {
-                "mode": CV2CvtGammaMode.POSTARIZE,
-                "gamma": 7
+                "mode": CV2CvtColorMode.RGB2LUV
             }
-        }
+        },
     ],
     path_img = ROOTPATH + "/images/test/gettyimages-1194679097.jpg",
-    # path_result = ROOTPATH + "/images/test/hoge.jpg",
+    # path_result = ROOTPATH + "/images/test/gettyimages-1194679097_cvtcolor_rgb2luv.jpg",
     display_progress_imgs = True
 )
